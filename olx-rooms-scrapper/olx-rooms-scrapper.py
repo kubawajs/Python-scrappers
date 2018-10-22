a1825fab3f5eaf19ff35a10aefdea3410a6c4a92
Jakub Wajs <kubawajs@interia.pl>
2018-10-22 22:30:07 +0200

    Removed advertisements and changed encoding

--- olx-rooms-scrapper/olx-rooms-scrapper.py
+++ olx-rooms-scrapper/olx-rooms-scrapper.py
@@ -2,12 +2,14 @@ from requests import get
 from bs4 import BeautifulSoup
 import csv
 
-# TODO: save to csv in loop
 # TODO: pagination
+# TODO: move city to parameter
+# TODO: move result file name to parameter
 
 #statics
 city = 'poznan'
-olx_url = 'https://www.olx.pl/nieruchomosci/stancje-pokoje/{0}/?view=list'.format(city)
+olx_base_url = 'https://www.olx.pl'
+olx_url = '{0}/nieruchomosci/stancje-pokoje/{1}/?view=list'.format(olx_base_url, city)
 file_name = 'olx_room_data.csv'
 
 #get data from website
@@ -27,12 +29,16 @@ offer_list = []
 
 #process offer
 for idx, url in enumerate(offer_urls):
+    # check if not an advertisement
+    if(olx_base_url not in url):
+        continue
+
     # get offer page
     response = get(url)
     offer_soup = BeautifulSoup(response.text, 'html.parser')
 
     # scrap data
-    offer_dict = {}
+    offer_dict = {} 
     location = offer_soup.find('a', class_='show-map-link').getText().split(',')
     offer_dict['city'] = location[0]
     offer_dict['region'] = location[1]
@@ -40,7 +46,7 @@ for idx, url in enumerate(offer_urls):
     offer_dict['price'] = offer_soup.find('div', class_='price-label').getText().strip('\n')
     
     raw_html_description = offer_soup.find('div', {'id': 'textContent', 'class': 'clr'}).getText()
-    offer_dict['description'] = "".join(line.strip() for line in raw_html_description.split("\n"))
+    offer_dict['description'] = "".join(line.strip() for line in raw_html_description.split('\n'))
 
     # parse data in table
     all_values_from_table = offer_soup.find_all('td', class_='value')
@@ -49,15 +55,13 @@ for idx, url in enumerate(offer_urls):
 
     # save to list
     offer_list.append(offer_dict)
-    print("Processing offer no " + str(idx + 1))
-    
-    if (idx + 1) >= 10:
-        break
+    print("Processing offer no {0}".format(str(idx + 1)))
+
 
 # save to file
-keys = ['city', 'region', 'district', 'description', 'price', 'Oferta od', 'Umeblowane', 'Rodzaj pokoju', 'Preferowani']
+keys = ['city', 'region', 'district', 'price', 'Oferta od', 'Umeblowane', 'Rodzaj pokoju', 'Preferowani', 'description']
 
-with open('olx_room_data.csv', 'w') as output_file:
+with open('olx_room_data.csv', 'w', newline='', encoding='utf-16') as output_file:
     dict_writer = csv.DictWriter(output_file, keys)
     dict_writer.writeheader()
     dict_writer.writerows(offer_list)
